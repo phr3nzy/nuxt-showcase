209ba90f2c0fc0983616e50a2d15487ff65320a2
phr3nzy <adilosama47@gmail.com>
2019-03-06 22:23:11 +0200

    edit(plugin): add new color scheme

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,9 +4,9 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#121212', // a color that is not in the material colors palette
+    primary: '#8e24aa', // a color that is not in the material colors palette
     accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
+		secondary: '#e91e63',
     info: colors.teal.lighten1,
     warning: colors.amber.base,
     error: colors.deepOrange.accent4,

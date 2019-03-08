176ca4f8df7c68793757bf42cd2ec1be742cf452
phr3nzy <adilosama47@gmail.com>
2019-03-08 11:34:14 +0200

    tweak(scroll): fix behavior of scroll & position and edit cards

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,43 @@
 <template>
   <v-layout column wrap>
-    <v-container
-      justify-center
-      align-center
-      class="white--text slight-br"
-      style="margin-top: 14rem;"
-    >
-      <v-layout align-center column justify-center>
-        <h1 class="display-3 font-weight-thick mb-3">Nuxify</h1>
-        <h3 class="subheading">where Power and Beauty meet</h3>
-        <v-btn to="/" color="white" class="secondary--text px-5" raised round ripple large>Discover</v-btn>
-      </v-layout>
-    </v-container>
-
-    <v-layout class="mb-5" row justify-center align-center>
-      <v-btn
-        @click="$el.scrollIntoView('2nd_layout')"
-        class="hvr-down"
-        huge
-        flat
-        fab
-        :ripple="false"
-        color="transparent"
+    <v-layout row>
+      <v-container
+        justify-center
+        align-center
+        class="white--text slight-br"
+        style="margin-top: 14rem;"
       >
+        <v-layout align-center column justify-center>
+          <h1 class="display-3 font-weight-thick mb-3">Nuxify</h1>
+          <h3 class="subheading">where Power and Beauty meet</h3>
+          <v-btn
+            to="/"
+            color="white"
+            class="secondary--text px-5"
+            raised
+            round
+            ripple
+            large
+          >Discover</v-btn>
+        </v-layout>
+      </v-container>
+    </v-layout>
+
+    <v-layout mb-5 row justify-center align-center>
+      <v-btn @click="scrollTo" class="hvr-down" huge flat fab :ripple="false" color="transparent">
         <v-icon color="white">expand_more</v-icon>
       </v-btn>
     </v-layout>
 
-    <v-layout v-el:2nd_layout class="my-5" id="2nd_layout" row justify-center align-center>
+    <v-layout class="my-5" row justify-center align-center>
       <v-item-group>
         <v-container grid-list-md>
           <v-layout row wrap>
-            <v-flex v-for="n in 3" :key="n" class="pa-5" xs12 sm4 md4 lg4>
+            <v-flex v-for="(item, index) in items" :key="index" class="pa-5" xs12 sm4>
               <v-item>
-                <v-card style="border-radius: 15px;"></v-card>
+                <v-card flat :elevation="24" style="border-radius: 25px;">
+                  <v-icon color="secondary" size="200">{{item.icon}}</v-icon>
+                </v-card>
               </v-item>
             </v-flex>
           </v-layout>
@@ -44,7 +48,25 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      items: [
+        { icon: 'whatshot' },
+        { icon: 'stars' },
+        { icon: 'favorite_border' }
+      ]
+    }
+  },
+  methods: {
+    scrollTo() {
+      this.$el.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
